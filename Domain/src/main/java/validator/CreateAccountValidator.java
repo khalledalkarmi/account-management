ea9d8a9c9ecb4cec32907093ddd640357f9d425d
Khaled Alkarmy <khaled.alkarmy@progressoft.com>
2023-03-06 11:15:40 +0300

    add test case for CreateAccountValidator

--- Domain/src/main/java/validator/CreateAccountValidator.java
+++ Domain/src/main/java/validator/CreateAccountValidator.java
@@ -4,6 +4,7 @@ import model.Account;
 import model.Customer;
 
 import java.util.List;
+import java.util.Objects;
 
 public class CreateAccountValidator {
 
@@ -11,10 +12,15 @@ public class CreateAccountValidator {
     private static List<Customer> customers = List.of(new Customer("KHALEDKAR"), new Customer("YOUSEFSUL"), new Customer("TAYSEERSAB"));
 
     public static boolean validate(Account account) {
+        if (Objects.isNull(account))
+            throw new NullPointerException("Invalid Account, account is null");
+
+        if (Objects.isNull(account.getId()))
+            throw new NullPointerException("Invalid Account id, account id is null");
         for (Customer customer : customers) {
             if (customer.getName().equals(account.getId()))
-                return false;
+                return true;
         }
-        return true;
+        return false;
     }
 }
