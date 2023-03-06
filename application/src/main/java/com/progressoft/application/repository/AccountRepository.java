908174a21078786236aeef46be19680052bf075a
Khaled Alkarmy <khaled.alkarmy@progressoft.com>
2023-03-06 14:21:29 +0300

    change service to repository

--- application/src/main/java/com/progressoft/application/repository/AccountRepository.java
+++ application/src/main/java/com/progressoft/application/repository/AccountRepository.java
@@ -1,11 +1,10 @@
 package com.progressoft.application.repository;
 
 import com.progressoft.application.entity.AccountEntity;
-import model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
-@Service
+@Repository
 public interface AccountRepository extends JpaRepository<AccountEntity, String> {
 
 }
