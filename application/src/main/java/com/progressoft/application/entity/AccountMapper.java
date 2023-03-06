8044a8b75b07477bd70c794dce92feabf1a28338
Khaled Alkarmy <khaled.alkarmy@progressoft.com>
2023-03-06 09:21:51 +0300

    add mapper

---
+++ application/src/main/java/com/progressoft/application/entity/AccountMapper.java
@@ -0,0 +1,18 @@
+package com.progressoft.application.entity;
+
+import model.Account;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class AccountMapper {
+    public AccountEntity map(Account account) {
+        return new AccountEntity.AccountEntityBuilder()
+                .customerId(account.getId())
+                .accountNumber(account.getAccountNumber())
+                .status(account.getStatus())
+                .creationDate(account.getCreationDate())
+                .availableBalance(account.getAvailableBalance())
+                .build();
+    }
+}
